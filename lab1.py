cd4fe183044da1b0148f929d1ca0202ab09152be
Scott-Dupasquier <sdupasqu@ualberta.ca>
2020-01-08 18:04:03 -0700

    Added downloading raw version from GitHub

--- lab1.py
+++ lab1.py
@@ -5,6 +5,6 @@ print(requests.__version__)
 google = requests.get("http://www.google.com")
 print(google)
 
-var = requests.get()
+var = requests.get("https://raw.githubusercontent.com/Scott-Dupasquier/404lab1/master/lab1.py")
 
 print(var.content)
